34c542a20aabc2ca95afdc71ef0f8ef449d58adc
uselessconsumer <eliottdale@gmail.com>
2023-08-17 17:23:01 -0400

    create solutions to the problems

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,29 @@
-const removeFromArray = function() {
 
-};
+
+
+
+const removeFromArray = function(array, ...args) {
+
+    let argsCount;
+    
+    for (argsCount = 0; argsCount < args.length ; ++argsCount) {
+        argsTest = args[argsCount];
+        console.log(argsTest);
+    
+        for (const element of array){
+            if (element === argsTest) {
+                let arrayIndex = array.indexOf(element);
+                delete array[arrayIndex]
+            }
+        }
+    
+        array = array.filter(() => true);
+        
+        }
+    
+            return array;
+    
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
