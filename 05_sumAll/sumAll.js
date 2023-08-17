f87390593264517426fb5262a67e34fc9fe2e099
uselessconsumer <eliottdale@gmail.com>
2023-08-17 18:18:08 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+
+    if ((Math.sign(a) != 1 || typeof(a) === 'string') || (Math.sign(b) != 1 || typeof(b) === 'string')) {
+        return 'ERROR';
+    }
+
+    if (a > b) {
+        var lowNum = b;
+        var highNum = a;
+    } else {
+        lowNum = a;
+        highNum = b;
+    }
+
+
+    let sum = 0;
+    let count = lowNum;
+    do {
+        sum = count + sum;
+        ++count;
+    } while (count <= highNum);
+    return sum;
+
+
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
